f17d35b71bcdf3814145abf007b7c3551065d6bf
Sönke Hahn <soenkehahn@gmail.com>
2014-10-07 15:52:46 +0800

    nix: use QuickCheck-2.7

--- default.nix
+++ default.nix
@@ -5,26 +5,4 @@
     baseNameOf path != "result" &&
     baseNameOf path != "dist") ./.
 }:
-let
-  # Force old QC for these
-  needsOldQC = [
-    "hspec"
-    "quickcheckIo"
-    "quickcheckInstances"
-    "enclosedExceptions"
-    "interpolate"
-  ];
-
-  haskellPackages = pkgs.haskellPackages.override {
-    extension = self: super: builtins.listToAttrs (map (name: {
-      inherit name;
-      value = super.${name}.override {
-        QuickCheck = self.QuickCheck_2_6;
-      };
-    }) needsOldQC);
-  };
-in
-
-(haskellPackages.buildLocalCabal src "kraken").override {
-  QuickCheck = haskellPackages.QuickCheck_2_6;
-}
+pkgs.haskellPackages.buildLocalCabal src "kraken"
