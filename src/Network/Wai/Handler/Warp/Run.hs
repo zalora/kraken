cdad5598f65db0d31d24e2841ec22c76f61ddfc1
Sönke Hahn <soenke.hahn@zalora.com>
2014-10-16 11:27:19 +0200

    daemon and kraken-web try to notify systemd when they've started up

--- src/Network/Wai/Handler/Warp/Run.hs
+++ src/Network/Wai/Handler/Warp/Run.hs
@@ -5,15 +5,32 @@ module Network.Wai.Handler.Warp.Run (
  ) where
 
 
+import           Data.Foldable
 import           Network.Wai
 import           Network.Wai.Handler.Warp
+import           System.Directory
 import           System.IO
+import           System.Process
 
 
 runWarp :: Port -> Application -> IO ()
 runWarp port application = do
   let settings =
         setPort port $
-        setBeforeMainLoop (hPutStrLn stderr ("listening on port " ++ show port)) $
+        setBeforeMainLoop (do
+          let message = "listening on port " ++ show port
+          hPutStrLn stderr message
+          systemdNotify message) $
         defaultSettings
   runSettings settings application
+
+systemdNotify :: String -> IO ()
+systemdNotify message = do
+  mExecutable <- findExecutable "systemd-notify"
+  forM_ mExecutable $ \ executable -> do
+    process <- spawnProcess executable $
+      "--ready" :
+      ("--status=" ++ message ++ "") :
+      []
+    _ <- waitForProcess process
+    return ()
