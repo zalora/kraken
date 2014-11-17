0d086e872033b65f4eb7ecb20015ac0fc90e2485
Sönke Hahn <soenke.hahn@zalora.com>
2014-11-17 18:50:21 +0800

    added servant as a submodule

--- default.nix
+++ default.nix
@@ -9,17 +9,10 @@ in
 , src ? filterHaskellSource ./.
 }:
 let
-  useLocalServant = false;
-  servantSrc = if useLocalServant
-    then filterHaskellSource <servant>
-    else pkgs.fetchgit {
-      url = "https://github.com/alpmestan/servant.git";
-      rev = "76ccb07e8d37f643a9abb1f14c57975af8f7d3c9";
-      sha256 = "fdf6d21b9c1fb81c4c135d37481d52b38a8162de74d1f7ffe2fc92e75ad65270";
-     };
+  servantRepo = ./servant;
   servant-new-impl = pkgs.haskellPackages.buildLocalCabalWithArgs {
     name = "servant";
-    src = "${servantSrc}/servant/";
+    src = "${servantRepo}/servant/";
   };
 in
 pkgs.haskellPackages.buildLocalCabalWithArgs {
